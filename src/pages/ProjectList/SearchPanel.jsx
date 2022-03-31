833690bcd20e339012445f683f1510cae2990baa
sheben404 <sheben404@gmail.com>
2022-03-31 18:01:34 +0800

    feat: 添加工程列表

---
+++ src/pages/ProjectList/SearchPanel.jsx
@@ -0,0 +1,34 @@
+export const SearchPanel = ({ users, param, setParam }) => {
+  return (
+    <form>
+      <div>
+        <input
+          type="text"
+          value={param.name}
+          onChange={(evt) =>
+            setParam({
+              ...param,
+              name: evt.target.value,
+            })
+          }
+        />
+        <select
+          value={param.personId}
+          onChange={(evt) =>
+            setParam({
+              ...param,
+              personId: evt.target.value,
+            })
+          }
+        >
+          <option value="">负责人</option>
+          {users.map((user, index) => (
+            <option key={index} value={user.id}>
+              {user.name}
+            </option>
+          ))}
+        </select>
+      </div>
+    </form>
+  );
+};
---
+++ src/pages/ProjectList/index.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { cleanObject } from "../../utils";
+import { List } from "./List";
+import { SearchPanel } from "./SearchPanel";
+import qs from "qs";
+
+const apiUrl = import.meta.env.VITE_API_URL;
+
+const ProjectListPage = () => {
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+
+  const [users, setUsers] = useState([]);
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    fetch(`${apiUrl}projects?${qs.stringify(cleanObject(param))}`).then(
+      async (res) => {
+        if (res.ok) {
+          setList(await res.json());
+        }
+      }
+    );
+  }, [param]);
+  useEffect(() => {
+    fetch(`${apiUrl}users`).then(async (res) => {
+      if (res.ok) {
+        setUsers(await res.json());
+      }
+    });
+  }, []);
+  return (
+    <div>
+      <SearchPanel users={users} param={param} setParam={setParam} />
+      <List users={users} list={list} />
+    </div>
+  );
+};
+
+export default ProjectListPage;
