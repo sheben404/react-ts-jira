bae4e1483fe210f5933b81cafecf8b5d4bcc957b
sheben404 <sheben404@gmail.com>
2022-04-02 22:12:23 +0800

    feat: 添加 Row 组件

--- src/AuthenticatedApp/index.tsx
+++ src/AuthenticatedApp/index.tsx
@@ -1,13 +1,43 @@
-import { Button } from "antd";
+import { Row } from "components/lib";
+import styled from "styled-components";
 import { useAuth } from "../context/AuthContext";
 import { ProjectListPage } from "../pages/ProjectList";
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr;
+  height: 100vh;
+`;
+
+const Header = styled(Row)`
+  justify-content: space-between;
+`;
+
+const HeaderLeft = styled(Row)``;
+
+const HeaderRight = styled.div``;
+
+const Main = styled.main`
+  height: calc(100vh - 6rem);
+`;
 
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
-    <div>
-      <Button onClick={logout}> 登出</Button>
-      <ProjectListPage />
-    </div>
+    <Container>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
+          <h2>Logo</h2>
+          <h2>项目</h2>
+          <h2>用户</h2>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>登出</button>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectListPage />
+      </Main>
+    </Container>
   );
 };
