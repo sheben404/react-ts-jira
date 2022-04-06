db0be5b8f165d47f383cc57cc3b35c77fb38568b
sheben404 <sheben404@gmail.com>
2022-04-06 09:50:06 +0800

    feat: 添加路由 projects 和 project

--- src/AuthenticatedApp/index.tsx
+++ src/AuthenticatedApp/index.tsx
@@ -1,5 +1,8 @@
 import { Button, Dropdown, Menu } from "antd";
 import { Row } from "components/lib";
+import { ProjectPage } from "pages/Project";
+import { Route, Routes } from "react-router";
+import { BrowserRouter as Router } from "react-router-dom";
 import styled from "styled-components";
 import { useAuth } from "../context/AuthContext";
 import { ProjectListPage } from "../pages/ProjectList";
@@ -24,32 +27,43 @@ const Main = styled.main`
   height: calc(100vh - 6rem);
 `;
 
-export const AuthenticatedApp = () => {
+const PageHeader = () => {
   const { logout, user } = useAuth();
   return (
+    <Header between={true}>
+      <HeaderLeft gap={true}>
+        <h2>Logo</h2>
+        <h2>项目</h2>
+        <h2>用户</h2>
+      </HeaderLeft>
+      <HeaderRight>
+        <Dropdown
+          overlay={
+            <Menu>
+              <Menu.Item key={"logout"}>
+                <a onClick={logout}>登出</a>
+              </Menu.Item>
+            </Menu>
+          }
+        >
+          <Button type="link">Hi, {user?.name}</Button>
+        </Dropdown>
+      </HeaderRight>
+    </Header>
+  );
+};
+
+export const AuthenticatedApp = () => {
+  return (
     <Container>
-      <Header between={true}>
-        <HeaderLeft gap={true}>
-          <h2>Logo</h2>
-          <h2>项目</h2>
-          <h2>用户</h2>
-        </HeaderLeft>
-        <HeaderRight>
-          <Dropdown
-            overlay={
-              <Menu>
-                <Menu.Item key={"logout"}>
-                  <a onClick={logout}>登出</a>
-                </Menu.Item>
-              </Menu>
-            }
-          >
-            <Button type="link">Hi, {user?.name}</Button>
-          </Dropdown>
-        </HeaderRight>
-      </Header>
+      <PageHeader />
       <Main>
-        <ProjectListPage />
+        <Router>
+          <Routes>
+            <Route path={"/projects"} element={<ProjectListPage />} />
+            <Route path={"/projects/:projectId/*"} element={<ProjectPage />} />
+          </Routes>
+        </Router>
       </Main>
     </Container>
   );
