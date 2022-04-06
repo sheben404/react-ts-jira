423c26ed111f5d890066ff4ee702bddc69b69425
sheben404 <sheben404@gmail.com>
2022-04-06 14:46:11 +0800

    fix: 使用 useMemo 解决无限渲染的问题

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,11 +1,14 @@
+import { useMemo } from "react";
 import { useSearchParams } from "react-router-dom";
 
 export const useUrlQueryParam = <K extends string>(keys: K[]) => {
   const [searchParams, setSearchParams] = useSearchParams();
   return [
-    keys.reduce((prev, key) => {
-      return { ...prev, [key]: searchParams.get(key) || "" };
-    }, {} as { [key in K]: string }),
+    useMemo(() => {
+      return keys.reduce((prev, key) => {
+        return { ...prev, [key]: searchParams.get(key) || "" };
+      }, {} as { [key in K]: string });
+    }, [searchParams]),
     setSearchParams,
   ] as const;
 };
