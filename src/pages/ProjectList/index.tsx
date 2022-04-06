0dc4ed2aa2910b289cbbeb6b86c9b59ebba2960d
sheben404 <sheben404@gmail.com>
2022-04-06 14:19:46 +0800

    feat: 实现 useUrlQueryParam hook（存在无限刷新页面的 bug）

--- src/pages/ProjectList/index.tsx
+++ src/pages/ProjectList/index.tsx
@@ -6,16 +6,18 @@ import styled from "styled-components";
 import { Typography } from "antd";
 import { useProjects } from "utils/projects";
 import { useUsers } from "utils/users";
+import { useUrlQueryParam } from "utils/url";
 
 const Container = styled.div`
   padding: 3.2rem;
 `;
 
 export const ProjectListPage = () => {
-  const [param, setParam] = useState({
+  const [, setParam] = useState({
     name: "",
     personId: "",
   });
+  const [param] = useUrlQueryParam(["name", "personId"]);
   const debouncedParam = useDebounce(param, 500);
   const { loading, error, list } = useProjects(debouncedParam);
   const { users } = useUsers();
