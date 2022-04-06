b9801e501f4d36ecfa6fe43eaa740f240c65f343
sheben404 <sheben404@gmail.com>
2022-04-06 09:14:08 +0800

    refactor: 使用 useRef 重构 useDocumentTitle

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
 
@@ -37,7 +37,7 @@ export const useDocumentTitle = (
   title: string,
   keepOnUnmount: boolean = true
 ) => {
-  const oldTitle = document.title;
+  const oldTitle = useRef(document.title).current;
   useEffect(() => {
     document.title = title;
   }, [title]);
@@ -47,5 +47,5 @@ export const useDocumentTitle = (
         document.title = oldTitle;
       }
     };
-  }, []);
+  }, [oldTitle, keepOnUnmount]);
 };
